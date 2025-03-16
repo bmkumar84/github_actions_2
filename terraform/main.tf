d095b0d57f0da1c5cfa1745ec14d3858b4addbc7
bmkumar84 <134595300+bmkumar84@users.noreply.github.com>
2025-03-16 22:24:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
 resource "azurerm_virtual_network" "my_terraform_network" {
   name                = "${random_pet.prefix.id}-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
+  location            = var.resource_group_location
   resource_group_name = rg_sb_eastus_128245_1_174213598323
 }
 
@@ -31,7 +31,7 @@ resource "azurerm_subnet" "my_terraform_subnet" {
 # Create public IPs
 resource "azurerm_public_ip" "my_terraform_public_ip" {
   name                = "${random_pet.prefix.id}-public-ip"
-  location            = azurerm_resource_group.rg.location
+  location            = var.resource_group_location
   resource_group_name = rg_sb_eastus_128245_1_174213598323
   allocation_method   = "Dynamic"
 }
@@ -39,7 +39,7 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
 # Create Network Security Group and rules
 resource "azurerm_network_security_group" "my_terraform_nsg" {
   name                = "${random_pet.prefix.id}-nsg"
-  location            = azurerm_resource_group.rg.location
+  location            = var.resource_group_location
   resource_group_name = rg_sb_eastus_128245_1_174213598323
 
   security_rule {
@@ -69,7 +69,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
 # Create network interface
 resource "azurerm_network_interface" "my_terraform_nic" {
   name                = "${random_pet.prefix.id}-nic"
-  location            = azurerm_resource_group.rg.location
+  location            = var.resource_group_location
   resource_group_name = rg_sb_eastus_128245_1_174213598323
 
   ip_configuration {
@@ -89,7 +89,7 @@ resource "azurerm_network_interface_security_group_association" "example" {
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "my_storage_account" {
   name                     = "diag${random_id.random_id.hex}"
-  location                 = azurerm_resource_group.rg.location
+  location                 = var.resource_group_location
   resource_group_name      = rg_sb_eastus_128245_1_174213598323
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -101,7 +101,7 @@ resource "azurerm_windows_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
   admin_username        = "azureuser"
   admin_password        = random_password.password.result
-  location              = azurerm_resource_group.rg.location
+  location              = var.resource_group_location
   resource_group_name   = rg_sb_eastus_128245_1_174213598323
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
   size                  = "Standard_DS1_v2"
