3ce9e8d90f5100386623a8be5909446a179a90f4
bmkumar84 <134595300+bmkumar84@users.noreply.github.com>
2025-03-16 22:29:06 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,12 @@ variable "resource_group_location" {
   description = "Location of the resource group."
 }
 
+variable "resource_group_name" {
+  default     = "rg_sb_eastus_128245_1_174213598323"
+  description = "Location of the resource group."
+}
+
+
 variable "prefix" {
   type        = string
   default     = "win-vm-iis"
